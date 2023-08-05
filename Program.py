d1966523ba444ba5460849ed68ec67b476619d33
NourhanAdelis1 <138725302+NourhanAdelis1@users.noreply.github.com>
2023-08-05 22:18:34 +0000

    ,ki8 bioj67yu

--- Program.py
+++ Program.py
@@ -1,1 +1,32 @@
+def add(a,b):
+    return a+b
 
+x = int(input("Enter the first number"))
+y = int(input("Enter the second number"))
+
+def sub(a,b):
+    return a-b
+
+
+def multi(a,b):
+    return a*b
+
+
+def div(a,b):
+    return a/b
+def calculator(operation,x,y):
+    match operation:
+        case "add":
+            return x + y
+        case "subtract":
+            return x - y
+        case "multiply":
+            return x * y
+        case "divide":
+            return x / y
+
+operation = input("Please pick an operation")
+
+print(calculator(operation, x, y))
+
+    
